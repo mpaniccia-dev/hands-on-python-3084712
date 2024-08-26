62885831f478a240880ac4e910a967a5c53b2d11
mpaniccia-dev <72767237+mpaniccia-dev@users.noreply.github.com>
2024-08-26 20:23:52 +0000

    python hands-on intro 2

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -1,6 +1,24 @@
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
+JOHN = NAMES[0]
+PAUL = NAMES[1]
+
+JOHN_PAUL = NAMES[:2]  #TO THE LEFT OF 2
+GEORGE_RINGO = NAMES[2:] #TO THE RIGHT OF 2
+REVERSE = NAMES[::-1] #START:STOP:STEP  = NO START, NO STOP, REVERSE STEP
+EVERY_OTHER = NAMES[::-2]
+
+print(sum(AGES))
+print(min(AGES))
+print(max(AGES))
+
+print(JOHN_PAUL)
+print(GEORGE_RINGO)
+print(REVERSE)
+
+
+"""
 i = 0
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
@@ -16,6 +34,7 @@ for name in reversed(NAMES):
     print(name)
 
 for i in range(5):
-    print(i)
+    print(i)    
+"""
 
 # enumerate
